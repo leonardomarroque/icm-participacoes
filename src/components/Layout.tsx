9df86cc922cc9dd6be5997feee2d6fb171c3bac7
leonardomarroque <leonardo.marroque@gmail.com>
2023-03-25 16:01:39 -0300

    Alteração no layout e título das páginas

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 export default function Layout (props: LayoutProps) {
     return (
         <div className={`
-            flex flex-col w-full md:w-11/12 lg:w-2/3
+            flex flex-col w-11/12 lg:w-2/3
             bg-white text-gray-800 rounded-md
         `}>
             <Titulo>{props.titulo}</Titulo>
